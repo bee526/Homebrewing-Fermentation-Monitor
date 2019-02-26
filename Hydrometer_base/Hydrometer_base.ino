4552dc7aa81c9d6a7c4f1cc33c0465dc58207c37
mjferro <47229527+mjferro@users.noreply.github.com>
2019-02-25 22:04:43 -0800

    Update Hydrometer_base.ino
    Implemented screen updates, need to re-add original function for backwards compatibility before pushing to master

--- Hydrometer_base/Hydrometer_base.ino
+++ Hydrometer_base/Hydrometer_base.ino
@@ -17,18 +17,22 @@
 
    01/28/2019 - REV 2
    Commented CODE and Reorginized
+   
+   02/25/2019
+   Updated screen to new format, need to add original screen function back in for backwards compatibility
+   TO DO: Add sd card detection
 
 
    TO DO
    Add message error checking, Call and Responce
    Improve touch detection
-   Improve on Screen graphics 
+   Improve on Screen graphics
    Add Fermintation proces tracking and graphing
    Add Calibration steps
    Save data to SD card
    Add HTTP Web interface
    Add AP_STA, connect to internet
-   
+
 */
 
 
@@ -37,9 +41,13 @@
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
 
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ILI9341.h>
+#include <Adafruit_GFX.h>         // Core graphics library
+#include <Fonts/FreeSerifItalic24pt7b.h>
+#include <Adafruit_ImageReader.h> // Image-reading functions
+#include <Adafruit_ILI9341.h>     // Hardware-specific library
 #include <XPT2046_Touchscreen.h>
+#include <SPI.h>
+#include <SD.h>
 
 
 /**************************** DEFINITIONS AND VARIABLES ********************************/
@@ -58,6 +66,7 @@ IPAddress ClientIP(192, 168, 4, 2);
 
 
 // DEFINE PIN DEFINITIONS
+#define SD_CS              D8
 #define TFT_DC             D8
 #define TFT_CS             D0
 #define TOUCH_CS           D3
@@ -66,10 +75,11 @@ IPAddress ClientIP(192, 168, 4, 2);
 // SCK D5
 ADC_MODE(ADC_VCC);
 
-
 // DEFINE VARIABLE DEFINITIONS
 const int BUFFER_SIZE = 512;     // Buffer used to for packaging message JSON
-char* commandState = "Standby";  // Default state
+char* commandState = "Standby";  // Default state (Standby, Fermentation, Calibration)
+bool sensorConnected = false;
+int sensorLastUpdate = -1;
 int sensorSleepInterval = 30;    // Default sleep interval in seconds
 int sensorSampleNumber = 0;      // Default number of samples
 
@@ -96,6 +106,7 @@ int sensorSignal;
 WiFiUDP udp;
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 XPT2046_Touchscreen ts(TOUCH_CS);
+Adafruit_ImageReader reader;
 
 /********************************** START SETUP ****************************************/
 void setup() {
@@ -107,8 +118,15 @@ void setup() {
   Serial.println("Initializing touch screen...");
   ts.begin();
 
-  // Splash Screen 1 second
-  drawSplashScreen(1);
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("failed!");
+  }
+  Serial.println("OK!");
+
+
+  // Splash Screen 2 second
+  drawSplashScreen(2);
 
   Serial.println("Start WiFi Access Point");
   WiFi.mode(WIFI_AP);
@@ -140,14 +158,14 @@ void loop() {
   // Check for incoming messages
   checkMessageReceived();
 
+  // Check that the sensor has not timed out
+  checkSensorState();
+
   // draw screen
   if (screen == 0) {
-    //drawWifiQuality();
-    drawButtons();
+    drawMainScreen();
   } else if (screen == 1) {
-    drawAbout();
-  } else if (screen == 2) {
-    drawSensorValues();
+    drawSettingsScreen();
   }
 
 }
@@ -187,7 +205,18 @@ void stateCalibration() {
 }
 
 
-
+/*
+ * Function to update the sensor state
+ * Checks to see if the sensor did not check in when it was expected to wake up
+ */
+void checkSensorState() {
+  if ((millis()/1000) - sensorLastUpdate < sensorSleepInterval + 60){
+    sensorConnected = true;
+  } else {
+    sensorConnected = false;
+  }
+  drawMainScreenSensorLast();
+}
 
 
 
@@ -197,60 +226,143 @@ void stateCalibration() {
  * Detect which button was pressed
  */
 void checkButtonTouch() {
+  int screenOld = screen;
   if (ts.touched()) {
-    refresh = true;
     if (!touchActive) {
       touchActive = true;
       TS_Point a = ts.getPoint();
-      Serial.println(a.x);
+      Serial.print("Screen Touched (");
+      Serial.print(a.x);
+      Serial.print(",");
+      Serial.print(a.y);
+      Serial.println(")");
 
-      // Screen 0 is divided into 6 buttons, bottom two are not used
       if (screen == 0) {
-        if (a.x >= 3250 && a.x <= 3700) {
-          Serial.println("Button 1, Standby Button Pressed");
-          stateStandby();
-        }
-        if (a.x >= 2700 && a.x <= 3150) {
-          Serial.println("Button 2, Fermentation Button Pressed");
-          stateFermentation();
-        }
-        if (a.x >= 2090 && a.x <= 2580) {
-          Serial.println("Button 3, Calibration Button Pressed");
-          stateCalibration();
-        }
-        if (a.x >= 1550 && a.x <= 2010) {
-          Serial.println("Button 4, About Button Pressed");
-          screen = 1;
-        }
-        if (a.x >= 960 && a.x <= 1450) {
-          Serial.println("Button 5, Data Button Pressed");
-          screen = 2;
-        }
-        if (a.x >= 350 && a.x <= 910) {
-          Serial.println("Button 6");
-        }
-
-      // Screen 1 only has the bottom button
-      } else if (screen == 1) {
-        if (a.x >= 350 && a.x <= 910) {
-          Serial.println("Button 6");
-          screen = 0;
-        }
-
-      // Screen 2 only has the bottom button
-      } else if (screen == 2) {
-        if (a.x >= 0350 && a.x <= 910) {
-          Serial.println("Button 6");
-          screen = 0;
-        }
+        mainScreenTouch(a.x, a.y);
+      } else
+      if (screen == 1) {
+        settingsScreenTouch(a.x, a.y);
       }
+
+      if (screen != screenOld) {
       tft.fillScreen(ILI9341_BLACK);
       refresh = true;
-
+      }
+      
     } else {
       touchActive = false;
     }
   }
+}
+
+
+/*
+ * Main Screen Touch Interface,
+ * Inputs the x,y touch
+ * Defines the area for each button
+ */
+void mainScreenTouch(int x, int y) {
+  // Settings Button Touch
+  if (x >= 3500 && x <= 3900 && y >= 200 && y <= 900) {
+    Serial.println("Main Screen, Settings Button Touched");
+    screen = 1;
+  }
+
+  // Bottom Button Touch
+  if (x >= 200 && x <= 900 && y >= 200 && y <= 4000) {
+    // if sensors is connected and not timed out
+    Serial.println("Main Screen, Bottom Button Touched");
+    Serial.println("Change State");
+    Serial.println("Start recording Fermentation");
+  }
+}
+
+
+/*
+ * Settings Screen Touch Interface,
+ * Inputs the x,y touch
+ * Defines the area for each button
+ */
+void settingsScreenTouch(int x, int y) {
+  drawSettingsScreen();
+  // Exit Button Touch (3500-3900,200-900)
+  if (x >= 3500 && x <= 3900 && y >= 200 && y <= 900) {
+    Serial.println("Settings Screen, Exit Button Touched");
+    screen = 0;
+  }
+
+  // ROW1COL1 Button Touch (2750-3350,3100-3850)
+  if (x >= 2750 && x <= 3350 && y >= 3100 && y <= 3850) {
+    Serial.println("Settings Screen, ROW1COL1 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 60);
+  }
+
+  // ROW1COL2 Button Touch (2750-3350,1100-1850)
+  if (x >= 2750 && x <= 3350 && y >= 1100 && y <= 1850) {
+    Serial.println("Settings Screen, ROW1COL2 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 60);
+  }
+
+  // ROW1COL3 Button Touch (2750-3350,200-1000)
+  if (x >= 2750 && x <= 3350 && y >= 200 && y <= 1000) {
+    Serial.println("Settings Screen, ROW1COL3 Button Touched");
+    reader.drawBMP("/SetG.bmp", tft, 190, 60);
+  }
+
+  // ROW2COL1 Button Touch (1950-2550,3100-3850)
+  if (x >= 1950 && x <= 2550 && y >= 3100 && y <= 3850) {
+    Serial.println("Settings Screen, ROW2COL1 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 130);
+  }
+
+  // ROW2COL2 Button Touch (1950-2550,1100-1850)
+  if (x >= 1950 && x <= 2550 && y >= 1100 && y <= 1850) {
+    Serial.println("Settings Screen, ROW2COL2 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 130);
+  }
+
+  // ROW2COL3 Button Touch (1950-2550,200-1000)
+  if (x >= 1950 && x <= 2550 && y >= 200 && y <= 1000) {
+    Serial.println("Settings Screen, ROW2COL3 Button Touched");
+    reader.drawBMP("/SetG.bmp", tft, 190, 130);
+  }
+
+  // ROW3COL1 Button Touch (1150-1750,3100-3850)
+  if (x >= 1150 && x <= 1750 && y >= 3100 && y <= 3850) {
+    Serial.println("Settings Screen, ROW3COL1 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 200);
+  }
+
+  // ROW3COL2 Button Touch (1150-1750,1100-1850)
+  if (x >= 1150 && x <= 1750 && y >= 1100 && y <= 1850) {
+    Serial.println("Settings Screen, ROW3COL2 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 200);
+  }
+
+  // ROW3COL3 Button Touch (1150-1750,200-1000)
+  if (x >= 1150 && x <= 1750 && y >= 200 && y <= 1000) {
+    Serial.println("Settings Screen, ROW3COL3 Button Touched");
+    reader.drawBMP("/SetG.bmp", tft, 190, 200);
+  }
+
+  // ROW4COL1 Button Touch (350-950,3100-3850)
+  if (x >= 350 && x <= 950 && y >= 3100 && y <= 3850) {
+    Serial.println("Settings Screen, ROW4COL1 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 270);
+  }
+
+  // ROW4COL2 Button Touch (350-950,1100-1850)
+  if (x >= 350 && x <= 950 && y >= 1100 && y <= 1850) {
+    Serial.println("Settings Screen, ROW4COL2 Button Touched");
+    reader.drawBMP("/SetR.bmp", tft, 190, 270);
+  }
+
+  // ROW4COL3 Button Touch (350-950,200-1000)
+  if (x >= 350 && x <= 950 && y >= 200 && y <= 1000) {
+    Serial.println("Settings Screen, ROW4COL3 Button Touched");
+    reader.drawBMP("/SetG.bmp", tft, 190, 270);
+  }
+
 }
 
 
@@ -295,22 +407,174 @@ void checkScreenSwipe() {
  */
 void drawSplashScreen(int seconds) {
   Serial.println("Start Screen Splash Screen");
-  tft.fillScreen(ILI9341_BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextColor(ILI9341_RED);
-  tft.setTextSize(4);
-  tft.println();
-  tft.println();
-  tft.println("Hydrometer");
-  tft.println();
-  tft.setTextColor(ILI9341_GREEN);
+  reader.drawBMP("/splash.BMP", tft, 0, 0);
   delay((seconds * 1000));
   tft.fillScreen(ILI9341_BLACK);
 }
 
 
 /*
- * Helper function to draw sext at location x,y
+ * Display Main Screen
+ * Gets the images from sd card using Adafruit_ImageReader objext
+ */
+void drawMainScreen() {
+  if (refresh) {
+    tft.fillScreen(ILI9341_BLACK);
+    reader.drawBMP("/Ferm.bmp", tft, 8, 8);
+    reader.drawBMP("/Settings.bmp", tft, 200, 5);
+    reader.drawBMP("/STATUS.bmp", tft, 10, 52);
+
+    // Status Tag
+    reader.drawBMP("/ReadyS.bmp", tft, 105, 45);               //TODO Tag based on status
+
+
+    reader.drawBMP("/CurSG.bmp", tft, 10, 90);
+    tft.setTextSize(2);
+    tft.setFont(&FreeSerifItalic24pt7b);
+    drawStrings(10, 175, "8.888");                             //TODO Input Value
+    tft.setFont();
+    tft.setTextSize(1);
+
+
+
+    reader.drawBMP("/OrigSG.bmp", tft, 10, 190);
+    drawStrings(100, 194, "8.888");                 //TODO Input Value
+
+    reader.drawBMP("/CurABV.bmp", tft, 10, 210);
+    drawStrings(100, 213, "100");                 //TODO Input Value
+
+    reader.drawBMP("/Update.bmp", tft, 10, 235);
+    reader.drawBMP("/BATT100.bmp", tft, 25, 254);
+    drawStrings(50, 258, "100%");                              //TODO Input Value
+
+    reader.drawBMP("/SIG100.bmp", tft, 125, 250);
+    drawStrings(150, 258, "100%");                             //TODO Input Value
+
+    reader.drawBMP("/START.bmp", tft, 10, 275);                //TODO Input Value
+
+    drawMainScreenValues();
+  }
+  refresh = false;
+
+}
+
+
+/*
+ * Display Main Screen Values
+ * Updates only the values and important information, minimizes re-draw time
+ */
+void drawMainScreenValues() {
+    tft.setTextColor(ILI9341_WHITE, ILI9341_BLACK);
+    tft.fillRect(10, 105, 225, 80, ILI9341_BLACK);
+    tft.setTextSize(2);
+    tft.setFont(&FreeSerifItalic24pt7b);
+    drawStrings(10, 175, "8.888");                             //TODO Input Value
+    tft.setFont();
+    tft.setTextSize(1);
+
+
+
+    // Original SG
+    drawStrings(100, 194, "8.888");                 //TODO Input Value
+
+    // Current ABV
+    drawStrings(100, 213, "100");                 //TODO Input Value
+
+    // Sensor Last Update
+    tft.fillRect(148, 233, 50, 10, ILI9341_BLACK);
+    drawMainScreenSensorLast();
+
+    // Sensor Battery
+    reader.drawBMP("/BATT100.bmp", tft, 25, 254);
+    tft.fillRect(48, 257, 50, 10, ILI9341_BLACK);
+    drawStrings(50, 258, String(sensorBattery));                              //TODO Input Value
+
+    // Sensor Signal
+    reader.drawBMP("/SIG100.bmp", tft, 125, 250);
+    tft.fillRect(148, 257, 50, 10, ILI9341_BLACK);
+    drawStrings(150, 258, String(sensorSignal));                             //TODO Input Value
+}
+
+
+/*
+ * Display Main Screen Values
+ * Print out the sensor connected update time
+ */
+void drawMainScreenSensorLast() {
+  if (sensorConnected) {
+    // Sensor Last Update
+    drawStrings(150, 234, String((millis() - (sensorLastUpdate*1000))/1000) + " SEC");
+  } else {
+    drawStrings(150, 234, "Not Connected");
+  }
+}
+
+
+/*
+ * Display Settings Screen 
+ * Gets the images from sd card using Adafruit_ImageReader objext
+ */
+void drawSettingsScreen() {
+  if (refresh) {
+    tft.fillScreen(ILI9341_BLACK);
+
+    reader.drawBMP("/SETMENU.bmp", tft, 8, 8);
+    reader.drawBMP("/X.bmp", tft, 200, 5);
+
+    drawStrings(10, 50, "Standby Interval (min)");
+    reader.drawBMP("/BlueL.bmp", tft, 5, 60);
+    tft.setTextSize(4);
+    drawStrings(60, 70, "888");                                //TODO Input Value
+    tft.setTextSize(1);
+    reader.drawBMP("/BlueR.bmp", tft, 135, 60);
+    reader.drawBMP("/SetG.bmp", tft, 190, 60);
+
+
+    drawStrings(10, 120, "Fermentation Interval (min)");
+    reader.drawBMP("/BlueL.bmp", tft, 5, 130);
+    tft.setTextSize(4);
+    drawStrings(60, 140, "888");                                //TODO Input Value
+    tft.setTextSize(1);
+    reader.drawBMP("/BlueR.bmp", tft, 135, 130);
+    reader.drawBMP("/SetG.bmp", tft, 190, 130);
+
+
+    drawStrings(10, 190, "Calibration Water (1.000)");
+    reader.drawBMP("/BlueL.bmp", tft, 5, 200);
+    tft.setTextSize(4);
+    drawStrings(60, 210, "888");                                //TODO Input Value
+    tft.setTextSize(1);
+    reader.drawBMP("/BlueR.bmp", tft, 135, 200);
+    reader.drawBMP("/SetG.bmp", tft, 190, 200);
+
+
+    drawStrings(10, 260, "Calibration Solution");
+    reader.drawBMP("/BlueL.bmp", tft, 5, 270);
+    tft.setTextSize(4);
+    drawStrings(60, 280, "888");                                //TODO Input Value
+    tft.setTextSize(1);
+    reader.drawBMP("/BlueR.bmp", tft, 135, 270);
+    reader.drawBMP("/SetG.bmp", tft, 190, 270);
+  }
+  refresh = false;
+
+  drawSettingsScreenValues();
+}
+
+
+
+/*
+ * Display Main Screen Values
+ * Updates only the values and important information, minimizes re-draw time
+ */
+void drawSettingsScreenValues() {
+  
+}
+
+
+
+/*
+ * Helper function to draw text at location x,y
  */
 void drawStrings(int x, int y, String text) {
   tft.setCursor(x, y);
@@ -556,6 +820,8 @@ void unpackageMessage(char* payload) {
 
   // Test if parsing succeeds.
   if (root.success()) {
+    sensorLastUpdate = millis()/1000;
+    drawMainScreenValues();
 
     // Unpack message name
     String message = root["message"];
